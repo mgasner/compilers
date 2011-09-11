73ba832c12de0a68ed43f0fcfa5e370625a09cf7
mgasner <max@naviasystems.com>
2011-09-11 19:07:36 -0400

    typo

--- ch1-straightline/straightline_prog.py
+++ ch1-straightline/straightline_prog.py
@@ -1,7 +1,7 @@
 import straightline_ast.py
 
 # a := 5 + 3;
-# b := (print(a, a - 1), 10 * a};
+# b := (print(a, a - 1), 10 * a);
 # print(b);
 
 prog = CompoundStm(AssignStm("a", OpExp(NumExp(5), OpExp.Plus, NumExp(3))), CompoundStm(AssignStm("b", EseqExp(PrintStm(PairExpList(IdExp("a"), LastExpList(OpExp(IdExp("a"), OpExp.Minus, NumExp(1))))), OpExp(NumExp(10), OpExp.Times, IdExp("a")))), PrintStm(LastExpList("b"))))
