25dc73ac52ca33bfa0767e97ababbe76c56c6981
mgasner <max@naviasystems.com>
2011-09-11 21:49:17 -0700

    corrected error in program text

--- ch1-straightline/straightline_prog.py
+++ ch1-straightline/straightline_prog.py
@@ -1,10 +1,11 @@
-import straightline_ast.py
+from straightline_ast import *
+from straightline_interp import *
 
 # a := 5 + 3;
 # b := (print(a, a - 1), 10 * a);
 # print(b);
 
-prog = CompoundStm(AssignStm("a", OpExp(NumExp(5), OpExp.Plus, NumExp(3))), CompoundStm(AssignStm("b", EseqExp(PrintStm(PairExpList(IdExp("a"), LastExpList(OpExp(IdExp("a"), OpExp.Minus, NumExp(1))))), OpExp(NumExp(10), OpExp.Times, IdExp("a")))), PrintStm(LastExpList("b"))))
+prog = CompoundStm(AssignStm("a", OpExp(NumExp(5), OpExp.Plus, NumExp(3))), CompoundStm(AssignStm("b", EseqExp(PrintStm(PairExpList(IdExp("a"), LastExpList(OpExp(IdExp("a"), OpExp.Minus, NumExp(1))))), OpExp(NumExp(10), OpExp.Times, IdExp("a")))), PrintStm(LastExpList(IdExp("b")))))
 
 maxargs(prog)
 interp(prog)
